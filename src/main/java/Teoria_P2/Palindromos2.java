698b92aec9ae271516071cc846f1ecc022b88a94
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-05-29 23:22:51 -0500

    Corrección aceptar sólo si "size1 == size2"

--- src/main/java/Teoria_P2/Palindromos2.java
+++ src/main/java/Teoria_P2/Palindromos2.java
@@ -14,26 +14,24 @@ public class Palindromos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String cadena = "abba";
+        String cadena = "aabaa";
         int aux = cadena.length() / 2;
         String subCadena1 = cadena.substring(0, aux);
         String subCadena2 = cadena.substring(aux, cadena.length());
         char letra1, letra2;
         int size1 = subCadena1.length();
         int size2 = subCadena2.length();
-        boolean condicion = true;
+        boolean condicion = size1 == size2;
 
         System.out.println("s1 " + subCadena1);
         System.out.println("s2 " + subCadena2);
-        
-        if (size1 == size2) {
-            for (int i = 0, j = size1-1; condicion && i < size1; i++, j--) {
-                System.out.println("i " + subCadena1.charAt(i));
-                System.out.println("j " + subCadena2.charAt(j));
-                condicion = subCadena1.charAt(i) == subCadena2.charAt(j);
-            }
+
+        for (int i = 0, j = size1 - 1; condicion && i < size1; i++, j--) {
+            System.out.println("i " + subCadena1.charAt(i));
+            System.out.println("j " + subCadena2.charAt(j));
+            condicion = subCadena1.charAt(i) == subCadena2.charAt(j);
         }
-        
+
         System.out.println("" + condicion);
     }
 
