95c001ab964312ac190df951f68b6ace79c1fef1
ggaby11 <ggaby11suarez.gomez@gmail.com>
2023-05-29 23:19:29 -0500

    Punto 3, código base

---
+++ src/main/java/Teoria_P2/Palindromos2.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Teoria_P2;
+
+/**
+ *
+ * @author Ggaby.sg
+ */
+public class Palindromos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cadena = "abba";
+        int aux = cadena.length() / 2;
+        String subCadena1 = cadena.substring(0, aux);
+        String subCadena2 = cadena.substring(aux, cadena.length());
+        char letra1, letra2;
+        int size1 = subCadena1.length();
+        int size2 = subCadena2.length();
+        boolean condicion = true;
+
+        System.out.println("s1 " + subCadena1);
+        System.out.println("s2 " + subCadena2);
+        
+        if (size1 == size2) {
+            for (int i = 0, j = size1-1; condicion && i < size1; i++, j--) {
+                System.out.println("i " + subCadena1.charAt(i));
+                System.out.println("j " + subCadena2.charAt(j));
+                condicion = subCadena1.charAt(i) == subCadena2.charAt(j);
+            }
+        }
+        
+        System.out.println("" + condicion);
+    }
+
+}
